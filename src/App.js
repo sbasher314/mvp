8a581e482b279713b30f782f15b573e3f84a1265
Steven Basher <sbasher314@gmail.com>
2021-04-06 23:08:48 -0400

    Setup environment Express, react, webpack

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+class App extends Component{
+   render(){
+      return(
+         <div>Placeholder</div>
+      );
+   }
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: './src/main.js',
+   mode: 'development',
+   module: {
+      rules: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/env']
+            }
+         },
+         {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+   },
+   resolve: { extensions: ["*", ".js", ".jsx"] },
+   output: {
+    path: path.join(__dirname, '/bundle'),
+    filename: 'index_bundle.js'
+  },
+   devServer: {
+    contentBase: path.join(__dirname, 'public/'),
+    publicPath: "http://localhost:9000/",
+    inline: true,
+    port: 9000
+  },
+
+   plugins:[
+      new HtmlWebpackPlugin({
+         template: './public/index.html'
+      })
+   ]
+}
